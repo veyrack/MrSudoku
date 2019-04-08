97001fe41379681b73890a8d4a1da2e655379166
PHAM <3521802@ppti-14-401-01.ufr-info-p6.jussieu.fr>
2019-04-08 19:40:17 +0200

    08/04

--- src/cljs/mrsudoku/model/solver.cljs
+++ src/cljs/mrsudoku/model/solver.cljs
@@ -1,5 +1,17 @@
 (ns mrsudoku.model.solver)
 
+(def graph {:A #{:F :B :C},
+            :B #{:C}
+            :C #{:D}
+            :D #{:E}
+            :E #{:C}
+            :F #{:H :G}
+            :G #{:H :I}
+            :H #{:F :I}
+            :I #{}})
+
+
+
 (defn solve
   "Solve the sudoku `grid` by returing a full solved grid,
  or `nil` if the solver fails."
@@ -26,7 +38,7 @@
 (defn select-var [doms]
   (some (fn [[x xdom]] (if (> (count xdom) 1)
                           x
-                          nil))doms))
+                          nil)) doms))
 
 (defn find-solution [constraints doms]
   (if (all-singleton? doms)
@@ -41,9 +53,40 @@
                   sol
                   (recur (rest xval))))
              nil)))))))
+(defn add-vect [graph vect val]
+  (if (contains? graph vect)
+    (assoc graph vect (conj (get graph vect) val))
+    (if (nil? val)
+      (assoc graph vect #{})
+      (assoc graph vect #{val}))))
+
+(defn transpose [graph]
+  (loop [keys (keys graph),values (vals graph),res {}]
+;    (println keys)
+;    (println values)
+;    (println res)
+    (if (seq keys)
+      (if (= #{} (first values))
+        (recur (rest keys) (rest values) res)
+        (recur (rest keys) (rest values) (adding res (first keys) (first values))))
+      (completing res graph))))
+
+(defn adding [graph key vals]
+  (loop [res graph,s vals]
+    (if (seq s)
+      (recur (add-vect res (first s) key) (rest s))
+      res)))
+
+(defn completing [res graph]
+  (loop [res res, s (keys graph)]
+    (if (seq s)
+      (if (contains? res (first s))
+        (recur res (rest s))
+        (recur (add-vect res (first s) nil) (rest s)))
+      res)))
 
 (defn add-edge [graph src dest]
-  (assoc grapg src (conj (get graph src) dest)))
+  (assoc graph src (conj (get graph src) dest)))
 
 (defn augment [graph src visited match]
   (loop [dests (get graph src),visited visited]
@@ -76,3 +119,41 @@
               (assoc ngraph vert #{})))
           graph
           verts))
+
+(defn dfs [grpah vert f init visited]
+  (if (visited vert)
+    [init visited]
+    (loop [verts (get graph vert), res (f init vert), visited (conj visited vert)]
+      (if (seq verts)
+        (let [[res', visited'] (dfs graph (first verts) f res visited)]
+          (recur (rest verts) res' visited'))
+        [res visited]))))
+
+(defn dfs-post [grpah vert f init visited]
+  (if (visited vert)
+    [init visited]
+    (loop [verts (get graph vert), res init,visited (conj visited vert)]
+      (if (seq verts)
+        (let [[res', visited'] (dfs-post graph (first verts) f res visited)]
+          (recur (rest verts) res' visited'))
+        [(f res vert) visited]))))
+
+(defn complete-matching? [vars match]
+  (= (count vars) (count match)))
+
+(defn dfs-stack [graph]
+  (loop [verts (keys graph),stack (),visited #{}]
+    (if (seq verts)
+      (let [[stack',visited'] (dfs-post graph (first verts) conj stack visited)]
+        (recur (rest verts) stack' visited'))
+      stack)))
+
+(defn compute-scc [graph]
+  (let [stack (dfs-stack graph), tgraph (transpose graph)]
+    (loop [s stack,visited #{},res []]
+      (if (seq s)
+        (if (visited (first s))
+          (recur (rest s) visited res)
+          (let [[comp visited'] (dfs tgraph (first s) conj #{} visited)];;dfs-pre si ca marche pas
+            (recur (rest s) visited' (conj res comp))))
+        res))))
